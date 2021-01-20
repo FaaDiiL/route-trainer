5e932df6bbcfce1996c40a69a634cd3784361d07
Fadil Al <fadil.alfarhan@gmail.com>
2021-01-20 08:39:13 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navigation from './Navigation';
+
+function App() {
+    return (
+        <>
+        <div>Welcome to React-router!</div>
+        <Navigation />
+        </>
+        );
+}
+
+export default App;
